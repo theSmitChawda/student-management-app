3bdc6f2dcea53ab41e495d893931bd2c20eab1bb
Smit Chawda <smitchawda@gmail.com>
2022-07-22 15:16:56 -0400

    Update functionality added

--- StudentManagementSystem/src/main/java/com/smitchawda/studentmanagementsystem/controller/StudentController.java
+++ StudentManagementSystem/src/main/java/com/smitchawda/studentmanagementsystem/controller/StudentController.java
@@ -1,12 +1,14 @@
 package com.smitchawda.studentmanagementsystem.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smitchawda.studentmanagementsystem.dao.StudentDao;
 import com.smitchawda.studentmanagementsystem.model.Student;
@@ -35,9 +37,33 @@ public class StudentController {
 		return "redirect:/students";
 	}
 	
-	@GetMapping("/students/update")
-	public String updateStudent(Model m) {
-		
+	@GetMapping("/students/update/{id}")
+	public String updateStudent(@PathVariable("id") Long id, Model m) {
+		m.addAttribute("student",repo.findById(id).get());
 		return "updateStudent";
+	}
+	
+	@PostMapping("/students/{id}")
+	public String update(@PathVariable("id") Long id, @ModelAttribute("student") Student student, Model m) {
+		Optional<Student> exStudent = repo.findById(id);
+		exStudent.get().setId(student.getId());
+		exStudent.get().setFirstName(student.getFirstName());
+		exStudent.get().setLastName(student.getLastName());
+		exStudent.get().setEmail(student.getEmail());
+		
+		repo.save(exStudent.get());
+		return "redirect:/students";
+	}
+	
+	@GetMapping("/students/delete/{id}")
+	public String removeStudent(@PathVariable("id") Long id ,Model m){
+		m.addAttribute("student",repo.findById(id).get());
+		return "deleteStudent";
+	}
+	
+	@PostMapping("/students/confirm-delete/{id}")
+	public String deleteStudent(@PathVariable("id") Long id) {
+		repo.deleteById(id);
+		return "redirect:/students";
 	}
 }
